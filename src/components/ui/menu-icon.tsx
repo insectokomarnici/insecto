27e725f2388704aba3e9143617ed53fa90deb46a
Insecto <insectokomarnici@gmail.com>
2026-09-17 22:20:18 +0200

    Match hamburger stroke weight to close

--- src/components/ui/menu-icon.tsx
+++ src/components/ui/menu-icon.tsx
@@ -4,9 +4,9 @@ import type { SVGProps } from "react";
 export function CarbonMenuThreeIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg viewBox="8 8 16 16" fill="currentColor" aria-hidden="true" focusable="false" {...props}>
-      <path d="M8 8.75h16v1.5H8z" />
-      <path d="M8 15.25h16v1.5H8z" />
-      <path d="M8 21.75h16v1.5H8z" />
+      <path d="M8 8.5h16v2H8z" />
+      <path d="M8 15h16v2H8z" />
+      <path d="M8 21.5h16v2H8z" />
     </svg>
   );
 }
